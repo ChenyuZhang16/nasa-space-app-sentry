6a4cb5bac4be5683b957ee87032aef314d92ff3e
ChenyuZhang16 <goodzhcy@gmail.com>
2021-10-03 15:52:24 +0100

    minor change

--- landslide_sentry/cache_manager/file_registry.py
+++ landslide_sentry/cache_manager/file_registry.py
@@ -16,7 +16,7 @@ EAST_TEXT_DETECTOR = {
     "key": "NASA_Landslide_Catalog_2008_2021.html",
     "download_name": "NASA_Landslide_Catalog_2008_2021.html",
     "name": "NASA_Landslide_Catalog_2008_2021.html",
-    "dir_path": "model",
+    "dir_path": "HTML",
     "url": "https://drive.google.com/u/0/uc?id=1zttpFp3vrHMqCk6jkQOTAX_cFd1NoxIG&export=download",
     "compression_method": None,
     "from_google_drive": True,
